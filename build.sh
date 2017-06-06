1fe30098e5eeb053d2ea49a165a4d86ac64320a8
Andreas Kruger <ak@pasientsky.no>
2017-06-06 13:27:48 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-mkdir bin
+export GOPATH=$(PWD)/packages/
+rm /bin/k8s-sslmate
 go get
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./bin/k8s-sslmate .
-cd bin
-zip k8s-sslmate.zip k8s-sslmate
-scp -4 k8s-sslmate.zip roffe@roffe.nu:/webroot/roffe/public_html/k8s-sslmate/
+
+if [ $? -eq 0 ]; then
+    git commit -am "Binary build"
+    git push
+fi
